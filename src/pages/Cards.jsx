08ba501ad34c282f7520edcf38f97423ecfd850e
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-07-18 11:30:00 +0530

    card

---
+++ src/pages/Cards.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Cards() {
+  return (
+    <div className="container my-3">
+      <h1 className="Trend">Trending Movie</h1>
+      <div
+        className="card bg-dark text-white shadow rounded"
+        style={{ width: "14rem" }}
+      >
+        <img
+          src="https://tse2.mm.bing.net/th/id/OIP.qkTYV2fJRgyssxhjrSuDbwHaLH?rs=1&pid=ImgDetMain&o=7&rm=3"
+          className="img-fluid"
+          alt="Movie"
+          style={{
+            height: "40vh",
+            objectFit: "cover",
+            borderRadius: "1rem",
+            margin: "5%",
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Banner from './Banner'
+import Cards from './Cards'
 
 function Home() {
   return (
     <div>
         <Banner />
-       
+         <Cards/>
     </div>
   )
 }
