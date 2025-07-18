8f6f29086b2d56d63b202f7a65689c879f8dd435
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-07-18 10:42:16 +0530

    banner sec

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -4,6 +4,7 @@ function Banner() {
   return (
     <div className='Titleimg'>
       <img src="https://images.alphacoders.com/111/1119553.jpg" alt="img" />
+       <p className='Titleimgtext'>Avengers Endgame</p>
     </div>
   )
 }
