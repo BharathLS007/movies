33635d0525552a47876e4f160d9b8fd4823fd93e
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-07-18 12:57:32 +0530

    ca

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -2,15 +2,11 @@ import React from "react";
 
 function Cards() {
   return (
-    <div className="container my-3">
-
-      <div
-        className="card bg-dark text-white shadow rounded "
-        style={{ width: "14rem" }}
-      >
-        <img
-          src="https://tse2.mm.bing.net/th/id/OIP.qkTYV2fJRgyssxhjrSuDbwHaLH?rs=1&pid=ImgDetMain&o=7&rm=3"
-          className="img-fluid"
+<div className="container my-3">
+  <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 moviecards">
+    <div className="col">
+      <div className="card bg-dark text-white shadow rounded">
+        <img src="https://tse2.mm.bing.net/th/id/OIP.qkTYV2fJRgyssxhjrSuDbwHaLH?rs=1&pid=ImgDetMain&o=7&rm=3" className="img-fluid"
           alt="Movie"
           style={{
             height: "40vh",
@@ -20,8 +16,12 @@ function Cards() {
           }}
         />
       </div>
-      
     </div>
+
+    {/* Repeat the .col div for more cards */}
+  </div>
+</div>
+
     
     
   );
