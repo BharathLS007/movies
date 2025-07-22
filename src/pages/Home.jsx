83ca946813489ed575423c1571a60be5892cab9a
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-07-22 10:28:59 +0530

    api done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Banner from "./Banner";
 import Cards from "./Cards";
 import axios from "axios";
 
 function Home() {
- 
-useEffect(() => {
-  axios
-    .get(
-      'https://api.themoviedb.org/3/discover/tv?api_key=ffbc70b1b2dd54b62933e5b8953c56a7&language=en'
-    )
-    .then(function (res) {
-      console.log(res.data.results); // Only logs the actual genre data
-    })
-    .catch(function (error) {
-      console.error('Error fetching genres:', error.message); // Cleaner error message
-    });
-}, []);
+  const [movie, setmovie] = useState([]);
 
-
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/discover/tv?api_key=ffbc70b1b2dd54b62933e5b8953c56a7&language=en"
+      )
+      .then(function (res) {
+        setmovie(res.data.results); // Only logs the actual genre data
+      })
+      .catch(function (error) {
+        console.error("Error fetching genres:", error.message); // Cleaner error message
+      });
+  }, []);
 
   return (
     <div>
-      
       <Banner />
       <h1 className="Trend">Trending Movie</h1>
 
       <div className="moviecontainer">
-        <Cards />
-
+        {movie.map((movieObj) => {
+          return <Cards key={movieObj.id} />;
+        })}
       </div>
     </div>
   );
 }
 
 export default Home;
-
 
 // second api credit      ffbc70b1b2dd54b62933e5b8953c56a7
