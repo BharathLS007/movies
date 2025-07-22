855f79312308df04a7cbeee89c916739cbbd9179
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-07-22 12:19:18 +0530

    updates on the way

--- src/pages/WatchList.jsx
+++ src/pages/WatchList.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import constructionGif from '../assets/Construction.gif'; // 👈 updated path
 
 function WatchList() {
   return (
-    <div>
-      <h1>Your WatchList</h1>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h2>This Page is Under Construction 🚧</h2>
+      <img
+        src={constructionGif}
+        alt="Under Construction"
+        style={{ width: '500px', borderRadius: '1rem' }}
+      />
     </div>
   );
 }
