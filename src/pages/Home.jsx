ce8e10c499825e3f1f781922578730fdd9a2d076
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-07-22 11:44:44 +0530

    pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
   useEffect(() => {
     axios
       .get(
-        "https://api.themoviedb.org/3/discover/tv?api_key=ffbc70b1b2dd54b62933e5b8953c56a7&language=en"
+        "https://api.themoviedb.org/3/discover/tv?api_key=ffbc70b1b2dd54b62933e5b8953c56a7&language=en&page=3"
       )
       .then(function (res) {
         setmovie(res.data.results); // Only logs the actual genre data
